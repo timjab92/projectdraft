1d05d38d612e221dc940cbb081c5c8d7d347313f
Tim Jablonski <timjab92@gmail.com>
2021-08-16 12:55:26 +0200

    first commit

---
+++ projectdraft/lib.py
@@ -0,0 +1,8 @@
+def try_me():
+    x = input("Do you have any doubts? Type y or n")
+    if x == "y":
+        return "No need to worry. You are amazing! Keep going!"
+    elif x == "n":
+        return "Thats exactly right"
+    else:
+        return "read the instructions."
